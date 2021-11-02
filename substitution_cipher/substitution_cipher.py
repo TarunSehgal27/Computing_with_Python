15a277f47463089d43b19094ff58497da7dce54e
Tarun Sehgal <tarunsehgal27@gmail.com>
2021-11-02 15:14:54 +0530

    Add file

---
+++ substitution_cipher/substitution_cipher.py
@@ -0,0 +1,20 @@
+import string
+dict={}
+data=""
+file=open("op_file.txt","w")
+for i in range(len(string.ascii_letters)):
+    dict[string.ascii_letters[i]]=string.ascii_letters[i-1]
+print(dict)
+with open("ip_file.txt") as f:
+    while True:
+        c=f.read(1)
+        if not c:
+            print("End of File")
+            break
+        if c in dict:
+            data=dict[c]
+        else:
+            data=c
+        file.write(data)
+        print(data)
+file.close()
