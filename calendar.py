d41f03c0cd674c5e44e1c8d821ff9dff4f79caf6
Tarun Sehgal <tarunsehgal27@gmail.com>
2021-11-30 12:57:02 +0530

    get calendar for year

---
+++ calendar.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 12:52:05 2021
+
+@author: TARUN
+"""
+
+import calendar
+year =int( input("Enter the year of the required calendar "))
+month = int( input("Enter the month of the required calendar "))
+print(calendar.month(year,month))
+print(calendar.calendar(year))
+print("Leap Year:",calendar.isleap(year))
